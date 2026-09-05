7207958f21f467c2d41dccce757d3d2a209a5b1d
Jay Baraiya <jb193y@gmail.com>
2026-09-05 12:50:41 -0500

    feat: add boot script to initialize valve controller and start main application

--- valve_controller/boot.py
+++ valve_controller/boot.py
@@ -11,6 +11,7 @@ time.sleep(4) # delay for serial connection stabilizer and thread prevention
 print(" Platform:", sys.platform)
 print(" CPU Frequency:", machine.freq() // 1000000, "MHz")
 gc.collect()
+print(" Total Memory:", gc.mem_total(), "bytes")
 print(" Free Memory:", gc.mem_free(), "bytes")
 
 print(">>> VALVE boot.py COMPLETED <<<")
